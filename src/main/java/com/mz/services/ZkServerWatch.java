acb8b5759afdd6ac5274a1092281f7f1ff119143
WangChong <mz1874@aliyun.com>
2023-03-07 20:12:02 +0800

    # 服务上下线模型完成

--- src/main/java/com/mz/services/ZkServerWatch.java
+++ src/main/java/com/mz/services/ZkServerWatch.java
@@ -1,5 +1,6 @@
 package com.mz.services;
 
+import cn.hutool.core.lang.UUID;
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
@@ -9,29 +10,35 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import static com.mz.services.ZkServerWatch.URL;
+
 /**
  * @author aiden
  * @data 07/03/2023
  * @description
  */
 public class ZkServerWatch {
-    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        List<String> children = null;
 
-        List<String> finalChildren = children;
-        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 2000, new Watcher() {
+    final static String URL = "linux1:2181,linux2:2181,linux3:2181";
+    static ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+         zooKeeper = new ZooKeeper(URL, 2000, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
-                Event.EventType type = event.getType();
-                // current path
-                System.out.println(type);
-                String path = event.getPath();
-                if (type == Event.EventType.NodeChildrenChanged){
-                    System.out.println("节点" + path + "上线了");
+                try {
+                    List<String> zooKeeperChildren = zooKeeper.getChildren("/services", true);
+                    System.out.println("当前存在的服务器节点");
+                    for (String zooKeeperChild : zooKeeperChildren) {
+                        System.out.println(zooKeeperChild);
+                    }
+                    System.out.println("---------------------------------------------------");
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                if (null != finalChildren){
 
-                }
             }
         });
         Stat exists = zooKeeper.exists("/services", false);
@@ -39,27 +46,26 @@ public class ZkServerWatch {
             String path = zooKeeper.create("/services", "storage services".getBytes(StandardCharsets.UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             System.out.println(path + "\t 创建成功");
         }
-
-        children = zooKeeper.getChildren("/services", true);
-        for (String node : children) {
-            System.out.println(node);
-        }
         TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
 }
 
 class Client {
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 2000, new Watcher() {
+        ZooKeeper zooKeeper = new ZooKeeper(URL, 2000, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
 
             }
         });
-        Random random = new Random(10000);
-        int number = random.nextInt();
-        String path = zooKeeper.create("/services/id_" + 4, String.valueOf(number).getBytes(StandardCharsets.UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-        zooKeeper.getChildren("/services", true);
-        TimeUnit.SECONDS.sleep(10);
+        UUID uuid = UUID.fastUUID();
+        zooKeeper.create("/services/id_" + uuid, uuid.toString().getBytes(StandardCharsets.UTF_8), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+        List<String> children = zooKeeper.getChildren("/services", true);
+        System.out.println("当前存在的服务器节点");
+        for (String zooKeeperChild : children) {
+            System.out.println(zooKeeperChild);
+        }
+        System.out.println("---------------------------------------------------");
+        TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
     }
 }
